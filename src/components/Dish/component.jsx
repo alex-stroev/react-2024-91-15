ec9fdb14d91b5f32416c443bb71e29e07f5febd5
Alex Stroev <stroev.alexander@gmail.com>
2024-01-28 19:06:19 +0300

    + dish counter

--- src/components/Dish/component.jsx
+++ src/components/Dish/component.jsx
@@ -1,11 +1,26 @@
 import styles from "./styles.module.scss";
+import { Button } from "../Button/component";
+import { useState } from "react";
+
 export const Dish = ({ dish }) => {
+    const [quantity, setQuantity] = useState(0);
     return (
         <div className={styles.dish}>
             <div className={styles.header}>
                 <div className={styles.name}>{dish.name}</div>
                 <div className={styles.price}>{dish.price}$</div>
             </div>
+
+            <div className={styles.counter}>
+                <Button disabled={quantity <= 0} onclick={() => setQuantity(quantity - 1)}>
+                    -
+                </Button>
+                <b>{quantity}</b>
+                <Button disabled={quantity >= 5} onclick={() => setQuantity(quantity + 1)}>
+                    +
+                </Button>
+            </div>
+
             {dish.ingredients && (
                 <div className={styles.ingredients}>
                     <div>
