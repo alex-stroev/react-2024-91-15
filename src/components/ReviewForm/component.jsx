5e70f63930b5119f1f6562673df4928c6fc80613
Alex Stroev <stroev.alexander@gmail.com>
2024-01-31 16:57:35 +0300

    + useCallback

--- src/components/ReviewForm/component.jsx
+++ src/components/ReviewForm/component.jsx
@@ -1,21 +1,21 @@
 import styles from "./styles.module.scss";
 import { useReviewForm } from "./useReviewForm";
 export const ReviewForm = () => {
-    const { form, setValue } = useReviewForm();
+    const { form, setName, setText, setRating } = useReviewForm();
     console.log(JSON.stringify(form, null, 2));
     return (
         <form className={styles.form}>
             <div className={styles.row}>
                 <label htmlFor="Name">Имя</label>
-                <input id="Name" type="text" value={form.name} onChange={(e) => setValue("setName", e)} />
+                <input id="Name" type="text" value={form.name} onChange={(e) => setName(e)} />
             </div>
             <div className={styles.row}>
                 <label htmlFor="Text">Текст</label>
-                <textarea id="Text" cols="30" rows="10" value={form.text} onChange={(e) => setValue("setText", e)}></textarea>
+                <textarea id="Text" cols="30" rows="10" value={form.text} onChange={(e) => setText(e)}></textarea>
             </div>
             <label htmlFor="Rating">Рейтинг</label>
             <div className={styles.row}>
-                <input id="Rating" type="number" min={1} max={5} value={form.rating} onChange={(e) => setValue("setRating", e)} />
+                <input id="Rating" type="number" min={1} max={5} value={form.rating} onChange={(e) => setRating(e)} />
             </div>
         </form>
     );
--- src/components/ReviewForm/useReviewForm.js
+++ src/components/ReviewForm/useReviewForm.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useCallback, useReducer } from "react";
 
 const initialValue = {
     name: "",
@@ -22,7 +22,13 @@ const reducer = (state, { type, payload }) => {
 export const useReviewForm = () => {
     const [form, dispatch] = useReducer(reducer, initialValue);
 
+    // А вот так вот с оберткой можно сделать? Вычисления выводим в setValue? Или тогда мемоизация теряет смысл?
+
     const setValue = (type, e) => dispatch({ type: type, payload: e.target.value });
 
-    return { form, setValue };
+    const setName = useCallback((e) => setValue("setName", e), []);
+    const setText = useCallback((e) => setValue("setText", e), []);
+    const setRating = useCallback((e) => setValue("setRating", e), []);
+
+    return { form, setName, setText, setRating };
 };
