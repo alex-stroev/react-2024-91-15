150d694e632b00a07e701281641fe5bf45cacd99
Alex Stroev <stroev.alexander@gmail.com>
2024-01-31 13:51:39 +0300

    tabs moved to separate component

--- src/components/Restaurants/component.jsx
+++ src/components/Restaurants/component.jsx
@@ -1,27 +1,29 @@
 import { useState } from "react";
 import { restaurants } from "../../materials/mock";
 import { Restaurant } from "../Restaurant/component";
-import { Button } from "../Button/component";
 import styles from "./styles.module.scss";
+import { RestaurantTabs } from "../RestaurantTabs/component";
 
 export const Restaurants = () => {
-    const [activeRestaurantIndex, setActiveRestaurantIndex] = useState(0);
+    const initialActiveRestaurantID = restaurants[0].id;
+    const [activeRestaurantID, setActiveRestaurantID] = useState(initialActiveRestaurantID);
+
     return (
         <div>
             <h1>Список ресторанов</h1>
-
             <nav className={styles.nav}>
-                {restaurants.map((restaurant, index) => (
-                    <Button
+                {restaurants.map((restaurant) => (
+                    <RestaurantTabs
+                        isCurrent={restaurant.id === activeRestaurantID}
                         onclick={() => {
-                            setActiveRestaurantIndex(index);
+                            setActiveRestaurantID(restaurant.id);
                         }}
                     >
                         {restaurant.name}
-                    </Button>
+                    </RestaurantTabs>
                 ))}
             </nav>
-            <Restaurant restaurant={restaurants[activeRestaurantIndex]} />
+            <Restaurant restaurant={restaurants.find((restaurant) => restaurant.id === activeRestaurantID)} />
         </div>
     );
 };
