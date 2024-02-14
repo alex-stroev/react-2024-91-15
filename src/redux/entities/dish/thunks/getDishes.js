4c8105bcc8170dbe31790c5e93d8ac3973e71ab2
Alex Stroev <stroev.alexander@gmail.com>
2024-02-14 16:27:39 +0300

    paused

--- src/redux/entities/dish/thunks/getDishes.js
+++ src/redux/entities/dish/thunks/getDishes.js
@@ -1,12 +1,19 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { selectDishIds } from "../selectors";
+import { selectRestaurantMenuById } from "../../restaurant/selectors";
 
 export const getDishes = createAsyncThunk(
-    "restaurant/getDishes",
+    "dish/getDishesByrestaurantId",
     async (restaurantId) => {
         const response = await fetch(`http://localhost:3001/api/dishes?restaurantId=${restaurantId}`);
         const result = await response.json();
         return result;
-    }
-    // { condition: (_, { getState }) => !selectDishIds(getState())?.length }
+    },
+    // {
+    //     condition: (restaurantId, { getState }) => {
+    //         const restaurantDishIds = selectRestaurantMenuById(getState(), restaurantId);
+    //         const dishIds = selectDishIds(getState());
+    //         return !restaurantDishIds.every((id) => dishIds.includes(id));
+    //     },
+    // }
 );
--- src/redux/entities/restaurant/selectors.js
+++ src/redux/entities/restaurant/selectors.js
@@ -1,3 +1,4 @@
 export const selectRestaurantModule = (state) => state.restaurant;
 export const selectRestaurantIds = (state) => selectRestaurantModule(state).ids;
 export const selectRestaurantById = (state, id) => selectRestaurantModule(state).entities[id];
+export const selectRestaurantMenuById = (state, id) => selectRestaurantById(state, id).menu;
